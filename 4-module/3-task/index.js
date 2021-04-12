d2e413429b1b10de20c25f920f41fcc7fe4f2ca6
Evgeniy <itsmeevg@gmail.com>
2021-04-12 10:35:13 +0300

    adding a solution for lesson 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+  for (const tr of table.rows) {
+    Array.from(tr.cells).forEach((td, index) => {
+      let trClassList = tr.classList;
+
+      if (index === 1) {
+        if (parseInt(td.textContent, 10) < 18) {
+          tr.style.textDecoration = 'line-through';
+        }
+      } else if (index === 2) {
+        if (td.textContent === 'm') {
+          trClassList.toggle('male', true);
+        } else if (td.textContent === 'f') {
+          trClassList.toggle('female', true);
+        }
+      } else if (index === 3) {
+        if (td.dataset.available === 'true') {
+          trClassList.toggle('available', true);
+        } else if (td.dataset.available === 'false') {
+          trClassList.toggle('unavailable', true);
+        } else if (!td.hasAttribute('data-available')) {
+          tr.hidden = true;
+        }
+      }
+    });
+  }
 }
