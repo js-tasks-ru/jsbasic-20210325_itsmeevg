af6e3e78931a76d4bc3c3eb15e34666502f3e347
Evgeniy <itsmeevg@gmail.com>
2021-04-26 09:30:10 +0300

    adding a solution for lesson 6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,6 +1,63 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let cardTop = document.createElement("div");
+    cardTop.classList.add("card__top");
+
+    let cardImage = document.createElement("img");
+    cardImage.classList.add("card__image");
+    cardImage.alt = "product";
+    cardImage.src = "/assets/images/products/" + product.image;
+    cardTop.append(cardImage);
+
+    let cardPrice = document.createElement("span");
+    cardPrice.classList.add("card__price");
+    cardPrice.textContent = "€" + product.price.toFixed(2);
+    cardTop.append(cardPrice);
+
+    card.append(cardTop);
+
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card__body");
+
+    let cardTitle = document.createElement("div");
+    cardTitle.classList.add("card__title");
+    cardTitle.textContent = product.name;
+
+    cardBody.append(cardTitle);
+
+    let button = document.createElement("button");
+    button.classList.add("card__button");
+    button.type = "button";
+
+    let icon = document.createElement("img");
+    icon.alt = "icon";
+    icon.src = "/assets/images/icons/plus-icon.svg";
+    button.append(icon);
+
+    cardBody.append(button);
+
+    let self = this;
+
+    button.addEventListener("click", function () {
+      this.dispatchEvent(
+        new CustomEvent("product-add", {
+          detail: self.product.id,
+          bubbles: true,
+        })
+      );
+    });
+
+    card.append(cardBody);
+
+    button.addEventListener("product-add", function (event) {});
+
+    this.elem = card;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,168 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+
+    const slidesWrapper = this.addArrows();
+
+    const carouselInner = this.slidesHandler();
+    slidesWrapper.append(carouselInner);
+
+    this.elem = slidesWrapper;
+
+    // this.clickHandler();
+  }
+
+  clickHandler() {
+    const rightArrow = document.querySelector(".carousel__arrow_right");
+    const leftArrow = document.querySelector(".carousel__arrow_left");
+    const slidesCount = document.querySelectorAll(".carousel__slide").length;
+    let currentSlide = 1;
+
+    rightArrow.addEventListener("click", function () {
+      document.querySelector(".carousel__inner").style.transform =
+        "translateX(-" + document.querySelector(".carousel__slide").offsetWidth * currentSlide + "px)";
+
+      currentSlide++;
+
+      if (slidesCount === currentSlide) {
+        this.style.display = "none";
+      } else {
+        leftArrow.style.display = "flex";
+      }
+    });
+
+    leftArrow.addEventListener("click", function () {
+      currentSlide--;
+      console.log(document.querySelector(".carousel__slide").offsetWidth * currentSlide - document.querySelector(".carousel__slide").offsetWidth);
+      console.log(currentSlide);
+      document.querySelector(".carousel__inner").style.transform =
+        "translateX(-" + (document.querySelector(".carousel__slide").offsetWidth * currentSlide - slideWidth) + "px)";
+
+      if (currentSlide === 1) {
+        this.style.display = "none";
+      } else {
+        rightArrow.style.display = "flex";
+      }
+    });
+  }
+
+  addArrows() {
+    let slidesWrapper = document.createElement("div");
+    slidesWrapper.classList.add("carousel");
+
+    let arrowRight = document.createElement("div");
+    arrowRight.classList.add("carousel__arrow");
+    arrowRight.classList.add("carousel__arrow_right");
+
+    let arrowRightIcon = document.createElement("img");
+    arrowRightIcon.alt = "icon";
+    arrowRightIcon.src = "/assets/images/icons/angle-icon.svg";
+    arrowRight.append(arrowRightIcon);
+
+    slidesWrapper.append(arrowRight);
+
+    let arrowLeft = document.createElement("div");
+    arrowLeft.classList.add("carousel__arrow");
+    arrowLeft.classList.add("carousel__arrow_left");
+    arrowLeft.style.display = "none";
+
+    let arrowLeftIcon = document.createElement("img");
+    arrowLeftIcon.alt = "icon";
+    arrowLeftIcon.src = "/assets/images/icons/angle-left-icon.svg";
+    arrowLeft.append(arrowLeftIcon);
+
+    slidesWrapper.append(arrowLeft);
+    let currentSlide = 1;
+
+    arrowRight.addEventListener("click", function () {
+      const slideWidth = document.querySelector(".carousel__slide").offsetWidth;
+
+      document.querySelector(".carousel__inner").style.transform =
+        "translateX(-" + slideWidth * currentSlide + "px)";
+
+      currentSlide++;
+
+      if (document.querySelectorAll(".carousel__slide").length === currentSlide) {
+        this.style.display = "none";
+      } else {
+        arrowLeft.style.display = "flex";
+      }
+    });
+
+    arrowLeft.addEventListener("click", function () {
+      const slideWidth = document.querySelector(".carousel__slide").offsetWidth;
+
+      currentSlide--;
+      
+      document.querySelector(".carousel__inner").style.transform =
+        "translateX(-" + (slideWidth * currentSlide - slideWidth) + "px)";
+
+      if (currentSlide === 1) {
+        this.style.display = "none";
+      } else {
+        arrowRight.style.display = "flex";
+      }
+    });
+
+    return slidesWrapper;
+  }
+
+  slidesHandler() {
+    let carouselInner = document.createElement("div");
+    carouselInner.classList.add("carousel__inner");
+
+    for (let i in this.slides) {
+      let carouselSlide = document.createElement("div");
+      carouselSlide.classList.add("carousel__slide");
+      carouselSlide.dataset.id = this.slides[i].id;
+
+      let carouselImg = document.createElement("img");
+      carouselImg.classList.add("carousel__img");
+      carouselImg.alt = "slide";
+      carouselImg.src = "/assets/images/carousel/" + this.slides[i].image;
+      carouselSlide.append(carouselImg);
+
+      let carouselCaption = document.createElement("div");
+      carouselCaption.classList.add("carousel__caption");
+
+      let carouselPrice = document.createElement("span");
+      carouselPrice.classList.add("carousel__price");
+      carouselPrice.textContent = "€" + this.slides[i].price.toFixed(2);
+      carouselCaption.append(carouselPrice);
+
+      let carouselTitle = document.createElement("div");
+      carouselTitle.classList.add("carousel__title");
+      carouselTitle.textContent = this.slides[i].name;
+      carouselCaption.append(carouselTitle);
+
+      let carouselButton = document.createElement("button");
+      carouselButton.classList.add("carousel__button");
+      carouselButton.type = "button";
+
+      let self = this;
+
+      carouselButton.addEventListener("click", function () {
+        this.dispatchEvent(
+          new CustomEvent("product-add", {
+            detail: self.slides[i].id,
+            bubbles: true,
+          })
+        );
+      });
+
+      let plusIcon = document.createElement("img");
+      plusIcon.alt = "icon";
+      plusIcon.src = "/assets/images/icons/plus-icon.svg";
+      carouselButton.append(plusIcon);
+
+      carouselCaption.append(carouselButton);
+
+      carouselSlide.append(carouselCaption);
+      carouselInner.append(carouselSlide);
+    }
+
+    return carouselInner;
   }
 }
